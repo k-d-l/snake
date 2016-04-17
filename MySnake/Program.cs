286aa8975a6a2b5e5f91ba1fe378ae6abcd0a36d
dk <k_d_l@ukr.net>
2016-04-17 16:41:35 +0300

    point class

--- MySnake/Program.cs
+++ MySnake/Program.cs
@@ -10,20 +10,20 @@ namespace MySnake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '0';
-            Draw(x1, y1, sym1);
-            int x2 = 3;
-            int y2 = 5;
-            char sym2 = '$';
-            Draw(x2, y2, sym2);
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '0';
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 2;
+            p2.y = 5;
+            p2.sym = '#';
+            p2.Draw();
+
             Console.ReadLine();
         }
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.WriteLine(sym);
-        }
+       
     }
 }
